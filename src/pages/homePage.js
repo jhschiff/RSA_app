b2a7836764217748736075d262d1a6e3fe086cfd
Jordan Schiff <jhschiff@umich.edu>
2025-02-18 12:21:54 -0700

    ran build

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -4,8 +4,8 @@ import Navbar from "../Navbar.js";
 
 import './homePage.css'
 
-import image1 from '../images/Image1.jpeg';
-import image2 from '../images/Image2.jpeg';
+// import image1 from '../images/Image1.jpeg';
+// import image2 from '../images/Image2.jpeg';
 
 export default function Homepage() {
   return (
@@ -16,12 +16,12 @@ export default function Homepage() {
       {/* Two Pictures Section */}
       <div className="homepage-images-container">
         <div className="image-wrapper">
-          <img src={image1} alt="Image 1" className="images" />
+          {/* <img src={image1} alt="Image 1" className="images" /> */}
           <p className="mt-2 text-gray-800">Image 1 Name</p>
         </div>
 
         <div className="image-wrapper">
-          <img src={image2} alt="Image 2" className="images" />
+          {/* <img src={image2} alt="Image 2" className="images" /> */}
           <p className="mt-2 text-gray-800">Image 2 Name</p>
         </div>
       </div>
